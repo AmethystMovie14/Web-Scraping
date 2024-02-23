5e2a77a13abd54ce7bedb462dffe6b57e2e09c0e
AmethystMovie14 <129354257+AmethystMovie14@users.noreply.github.com>
2024-02-23 07:54:38 -0600

    Funcional
    El codigo ya recopila datos

--- 06. Automatic-Web-Scraping/wather-selenium/weather.py
+++ 06. Automatic-Web-Scraping/wather-selenium/weather.py
@@ -4,29 +4,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 start_urls = ['https://www.accuweather.com/es/mx/mexico-city/242560/weather-forecast/242560',
-                    'https://www.accuweather.com/es/mx/guadalajara/243735/weather-forecast/243735',
-                    'https://www.accuweather.com/es/mx/monterrey/244681/weather-forecast/244681']
-
+              'https://www.accuweather.com/es/mx/guadalajara/243735/weather-forecast/243735',
+              'https://www.accuweather.com/es/mx/monterrey/244681/weather-forecast/244681']
 
 def extraer_clima():
-
     driver = webdriver.Edge()
 
     for url in start_urls:
         driver.get(url)
-
         time.sleep(2)
 
-        ciudad = driver.find_element_by_xpath('//h1').text
-        current = driver.find_element_by_xpath('//a[contains(@class, "cur-con-weather-card")]//div[@class="temp"]').text
-        real_feal = driver.find_element_by_xpath('//a[contains(@class, "cur-con-weather-card")]//div[@class="real-feel"]').text
+        ciudad = driver.find_element(By.XPATH, '//h1').text
+        current = driver.find_element(By.XPATH, '//a[contains(@class, "cur-con-weather-card")]//div[@class="temp"]').text
+        real_feal = driver.find_element(By.XPATH, '//a[contains(@class, "cur-con-weather-card")]//div[@class="real-feel"]').text
         
         real_feal = real_feal.replace('RealFeel®', '').replace('°','').replace('\n','').replace('\t','').replace('\r','').strip()
         current = current.replace('°','').replace('\n','').replace('\t','').replace('\r','').strip()
 
-        f = open("datos-clima.csv", "a")
-        f.write(f"{ciudad},{current},{real_feal}\n")
-        f.close()
+        with open("datos-clima.csv", "a") as f:
+            f.write(f"{ciudad},{current},{real_feal}\n")
 
         print(ciudad)
         print(current)
